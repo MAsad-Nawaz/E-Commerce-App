5c5da13026a393317456bf84cc51f70035369608
MAsad-Nawaz <asadnawaz148@gmail.com>
2022-04-22 18:24:47 +0500

    responsive desifn and complete site with static data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import './App.css';
-
+import Product from './pages/Product';
+import Home from './pages/Home';
+import ProductList from './pages/ProductList';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Cart from './pages/Cart';
 function App() {
   return (
-    <div className="App">
-      <h1>
-        Hello
-      </h1>
-    </div>
+    <Cart />
   );
 }
-
 export default App;
